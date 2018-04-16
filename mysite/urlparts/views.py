85847be1a72fe651977eb95f47b39cb5b5bebc6e
LarsVlyminckx <lars.vlyminckx@hotmail.com>
2018-04-16 16:26:42 +0200

    add to list

--- mysite/urlparts/views.py
+++ mysite/urlparts/views.py
@@ -6,11 +6,16 @@ import redis
 
 #sadd answers "It is certain" "It is decidedly so" "Without a doubt" "Yes definitely" "You may rely on it" "As I see it, yes" "Most likely" "Outlook good" "Yes" "Signs point to yes" "Reply hazy try again" "Ask again later" "Better not tell you now" "Cannot predict now" "Concentrate and ask again" "Don't count on it" "My reply is no" "My sources say no" "Outlook not so good" "Very doubtful"
 r = redis.StrictRedis(host='localhost', port=6379, db = 0);
-
+my_list  = []
 def index(request):
     answer = request.get_full_path()
     return render(request,'index.html',{'answer':answer});
 def andereurl(request):
     answer = request.get_full_path()
-    answer = answer.split('/', 1)[1]
-    return render(request,'index.html',{'answer':answer});
+    #if "//" in answer:
+    #    return render(request,'index.html',{'answer':'answer'})
+    #else:
+    answer = answer.split('/', 2)[2]
+    answer = answer.replace('/','---')
+    my_list .append(answer)
+    return render(request,'index.html',{'answer':my_list})
